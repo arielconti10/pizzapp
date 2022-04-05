b2c89bdc339fcb43a3f56ec5e354f0df87bec21c
Ariel <arielconti10@gmail.com>
2022-04-05 14:25:31 +0100

    feat: created SignIn page

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import { ThemeProvider } from 'styled-components/native'
 
 import theme from './src/theme';
+import { SignIn } from '@src/screens/SignIn';
 
 export default function App() {
   
@@ -21,7 +22,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Text>Hello World!</Text>
+      <SignIn />
     </ThemeProvider>
   );
 }
