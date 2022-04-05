b3865d4767482c7a1f7e28357686cd9959fa052b
Ariel <arielconti10@gmail.com>
2022-04-05 11:16:44 +0100

    feat: initial configuration of react native app

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@utils': './src/utils',
+            '@assets': './src/assets',
+          }
+        }
+      ]
+    ]
   };
 };
