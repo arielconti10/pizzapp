af66f2f7d6b5544d2ec21fe4514adc2678f8b194
Ariel <arielconti10@gmail.com>
2022-04-08 20:44:42 +0100

    feat: added button back component

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, TouchableOpacity } from 'react-native';
 
-import { Container } from './styles'
+import { ButtonBack } from '@components/ButtonBack';
+
+import { Container, Header, Title, DeleteLabel } from './styles'
+
 export function Product() {
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-
+      <Header>
+        <Title>Cadastrar</Title>
+        <TouchableOpacity>
+          <DeleteLabel>Deletar</DeleteLabel>
+        </TouchableOpacity>
+        <ButtonBack />
+      </Header>
     </Container>
   )
 }
