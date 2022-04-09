60ec28430d30282634e95968b268b4d0d4e25fdf
Ariel <arielconti10@gmail.com>
2022-04-09 14:58:48 +0100

    feat: created add product form

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { Platform, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Platform, TouchableOpacity, ScrollView } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
+import { Button } from '@components/Button';
 import { ButtonBack } from '@components/ButtonBack';
+import { InputPrice } from '@components/InputPrice';
+import { Input } from '@components/Input';
 import { Photo } from '@components/Photo';
 
 import { 
@@ -10,28 +14,77 @@ import {
   Title, 
   DeleteLabel,
   Upload,
-  PickImageButton
-} from './styles'
+  PickImageButton,
+  Form,
+  Label,
+  InputGroup,
+  InputGroupHeader,
+  MaxCharacters,
+} from './styles';
 
 export function Product() {
+  const [image, setImage] = useState('');
+
+  async function handlePickerImage() {  
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if(status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4],
+      })
+
+      if(!result.cancelled) {
+        setImage(result.uri)
+      }
+    }
+
+  }
+
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-      <Header>
-        <ButtonBack />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header>
+          <ButtonBack />
 
-        <Title>Cadastrar</Title>
-        
-        <TouchableOpacity>
-          <DeleteLabel>Deletar</DeleteLabel>
-        </TouchableOpacity>
+          <Title>Cadastrar</Title>
+          
+          <TouchableOpacity>
+            <DeleteLabel>Deletar</DeleteLabel>
+          </TouchableOpacity>
 
-      </Header>
+        </Header>
 
-      <Upload>
-        <Photo uri="https://github.com/arielconti10.png" />
-        <PickImageButton title="carregar" type="primary"/>
-      </Upload>
+        <Upload>
+          <Photo uri={image} />
+          <PickImageButton title="carregar" type="primary" onPress={handlePickerImage}/>
+        </Upload>
+        <Form>
+          <InputGroup>
+            <Label>Nome</Label>
+            <Input />
+          </InputGroup> 
 
+          <InputGroup>
+            <InputGroupHeader>
+              <Label>Descrição</Label>
+              <MaxCharacters>0 de 144 caracteres</MaxCharacters>
+            </InputGroupHeader>
+            <Input multiline maxLength={144} style={{ height: 60}} />
+          </InputGroup> 
+
+          <InputGroup>
+            <Label>Tamanhos e preços</Label>
+            <InputPrice size="P" />
+            <InputPrice size="M" />
+            <InputPrice size="G" />
+          </InputGroup>
+
+          <Button 
+            title="Cadastrar pizza"
+          />
+        </Form>
+      </ScrollView>
     </Container>
   )
 }
