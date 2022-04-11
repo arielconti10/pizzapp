3ed338efcb685c048758d09485bf11a1138be09d
Ariel <arielconti10@gmail.com>
2022-04-11 17:41:57 +0100

    feat: Created search component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native';
 import { useTheme } from 'styled-components/native';
+import firestore from '@react-native-firebase/firestore';
 
 import { 
   Container,
@@ -15,8 +16,44 @@ import {
   NewProductButton,
 } from './styles';
 
+import { Search } from '@components/Search';
+
 export function Home() {
   const { COLORS } = useTheme()
+  const [search, setSearch] = useState<string>('');
+  const [pizzas, setPizzas] = useState<{id: string;}[]>([]);
+
+
+  function fetchPizzas(value: string) {
+    const formattedValue = value.toLocaleLowerCase().trim();
+
+    firestore()
+      .collection('pizzas')
+      .orderBy('name_insensitive')
+      .startAt(formattedValue)
+      .endAt(`${formattedValue}\uf8ff`)
+      .get()
+      .then(response => {
+        const data = response.docs.map(doc => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          }
+        });
+
+        setPizzas(data);
+      })
+      .catch(() => Alert.alert('Consulta', 'Não foi possível realizar a consulta'));
+  }
+
+  function handleSearch() {
+    fetchPizzas(search);
+  }
+
+  function handleSearchClear() {
+    setSearch('');
+    fetchPizzas('');
+  }
 
   return (
     <Container>
@@ -29,8 +66,15 @@ export function Home() {
         <TouchableOpacity>
           <MaterialIcons name="logout" size={24} color={COLORS.TITLE} />
         </TouchableOpacity>
-
       </Header>
+
+      <Search
+        onChangeText={setSearch}
+        value={search}
+        onSearch={handleSearch}
+        onClear={handleSearchClear}
+      />
+
     </Container>
   )
 }
